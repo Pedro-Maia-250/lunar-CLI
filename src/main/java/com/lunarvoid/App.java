329528a9cb00157325fbb12a9b615b1dba693683
Pedro Maia <pedrodrm250@gmail.com>
2026-05-10 13:33:51 -0300

    adicionado conversão de tipos complexos -f 2.5.2-SNAPSHOT

--- src/main/java/com/lunarvoid/App.java
+++ src/main/java/com/lunarvoid/App.java
@@ -12,24 +12,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        if (args.length == 0) {
-            System.out.println("Espera-se argumentos --help para mais informações");
-            return;
-        }else{
-            Map<String,Comando> comandos = new HashMap<>();
+        try{
 
-            comandos.put("--version", new VersionComand());
-            comandos.put("--help", new HelpComand());
-            comandos.put("--convert", new ConvertComand());
-
-            Comando comando = comandos.get(args[0]);
-
-            if(comando == null){
-                System.out.println("Comando invalido --help para mais informações");
+            if (args.length == 0) {
+                System.out.println("Espera-se argumentos --help para mais informações");
                 return;
+            }else{
+                Map<String,Comando> comandos = new HashMap<>();
+                
+                comandos.put("--version", new VersionComand());
+                comandos.put("--help", new HelpComand());
+                comandos.put("--convert", new ConvertComand());
+                
+                Comando comando = comandos.get(args[0]);
+                
+                if(comando == null){
+                    System.out.println("Comando invalido --help para mais informações");
+                    return;
+                }
+                
+                comando.execute(args);
             }
-
-            comando.execute(args);
+        }catch(NumberFormatException e){
+            System.out.println("Entrada invalida para a base expecificada: " + e.getMessage());
+            return;
+        }catch(IllegalArgumentException e){
+            System.out.println("Entrada invalida: " + e.getMessage());
         }
+        
     }
 }
--- src/main/java/com/lunarvoid/comandos/ConvertComand.java
+++ src/main/java/com/lunarvoid/comandos/ConvertComand.java
@@ -10,35 +10,293 @@ public class ConvertComand implements Comando {
     public void execute(String[] args){
         String error = "";
 
-        if (args[1].isEmpty() || args[2].isEmpty()) {
-            error = "invalid sintax --convert X --Y or --help for informations";
-            return;
+        if(args[1].isBlank()){
+            error = "Sintaxe invalida --help para mais informações";
         }
-        else if (args[2].equals("--hex")){
-            System.out.println(convertDecimal(Integer.valueOf(args[1]), 16));
-        }
-        else if (args[2].equals("--oct")){
-            System.out.println(convertDecimal(Integer.valueOf(args[1]), 8));
-        }
-        else if (args[2].equals("--bin")){
-            System.out.println(convertDecimal(Integer.valueOf(args[1]), 2));
-        }
-        else if (args[2].equals("--base")){
-            if(!args[3].isEmpty()){
-                System.out.println(convertDecimal(Integer.valueOf(args[1]), Integer.valueOf(args[3])));
-            }else{
-                error = "sintax error --base X (X is a numeric base)";
-            }
-        }
-        else if (args[1].equals("--base")){
-            if(!args[2].isEmpty()){
-                System.out.println(values(Integer.valueOf(args[2])));
-            }else{
-                error = "sintax error --base X (X is a numeric base)";
-            }
-        }
-        else{
-            error = "invalid argument --help for informations";
+
+        switch (args[1]) {
+
+            case "--hex":
+
+                if (!args[2].isBlank() || !args[3].isBlank() || !args[4].isBlank()) {
+                    error = "Sintaxe invalida --help para mais informações";
+                }
+
+                
+                switch (args[3]) {
+                    case "--hex": {
+
+                        String dec = convertToDecimal(args[2], 16);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 16));
+                        return;
+                    }
+                    case "--dec": {
+                        String dec = convertToDecimal(args[2], 16);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 10));
+                        return;
+                    }
+                    case "--oct": {
+                        String dec = convertToDecimal(args[2], 16);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 8));
+                        return;
+                    }
+                    case "--bin": {
+                        String dec = convertToDecimal(args[2], 16);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 2));
+                        return;
+                    }
+                    case "--base": {
+                        if(!args[4].isBlank()){
+                            String dec = convertToDecimal(args[2], 16);
+                            System.out.println(convertDecimal(Integer.valueOf(dec), Integer.valueOf(args[4])));
+                            return;
+                        }else{
+                            error = "Sintaxe invalida --help para mais informações";
+                            break;
+                        }
+                    }
+                
+                    default:
+                        error = "Sintaxe invalida --help para mais informações";
+                    break;
+                }
+            break;
+
+            case "--dec":
+
+                if (!args[2].isBlank() || !args[3].isBlank() || !args[4].isBlank()) {
+                    error = "Sintaxe invalida --help para mais informações";
+                }
+
+                
+                switch (args[3]) {
+                    case "--hex": {
+
+                        String dec = convertToDecimal(args[2], 10);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 16));
+                        return;
+                    }
+                    case "--dec": {
+                        String dec = convertToDecimal(args[2], 10);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 10));
+                        return;
+                    }
+                    case "--oct": {
+                        String dec = convertToDecimal(args[2], 10);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 8));
+                        return;
+                    }
+                    case "--bin": {
+                        String dec = convertToDecimal(args[2], 10);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 2));
+                        return;
+                    }
+                    case "--base": {
+                        if(!args[4].isBlank()){
+                            String dec = convertToDecimal(args[2], 10);
+                            System.out.println(convertDecimal(Integer.valueOf(dec), Integer.valueOf(args[4])));
+                            return;
+                        }else{
+                            error = "Sintaxe invalida --help para mais informações";
+                            break;
+                        }
+                    }
+                
+                    default:
+                        error = "Sintaxe invalida --help para mais informações";
+                    break;
+                }
+            break;
+
+            case "--oct":
+
+                if (!args[2].isBlank() || !args[3].isBlank() || !args[4].isBlank()) {
+                    error = "Sintaxe invalida --help para mais informações";
+                }
+
+                
+                switch (args[3]) {
+                    case "--hex": {
+
+                        String dec = convertToDecimal(args[2], 8);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 16));
+                        return;
+                    }
+                    case "--dec": {
+                        String dec = convertToDecimal(args[2], 8);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 10));
+                        return;
+                    }
+                    case "--oct": {
+                        String dec = convertToDecimal(args[2], 8);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 8));
+                        return;
+                    }
+                    case "--bin": {
+                        String dec = convertToDecimal(args[2], 8);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 2));
+                        return;
+                    }
+                    case "--base": {
+                        if(!args[4].isBlank()){
+                            String dec = convertToDecimal(args[2], 8);
+                            System.out.println(convertDecimal(Integer.valueOf(dec), Integer.valueOf(args[4])));
+                            return;
+                        }else{
+                            error = "Sintaxe invalida --help para mais informações";
+                            break;
+                        }
+                    }
+                
+                    default:
+                        error = "Sintaxe invalida --help para mais informações";
+                    break;
+                }
+            break;
+
+            case "--bin":
+
+                if (!args[2].isBlank() || !args[3].isBlank() || !args[4].isBlank()) {
+                    error = "Sintaxe invalida --help para mais informações";
+                }
+
+                
+                switch (args[3]) {
+                    case "--hex": {
+
+                        String dec = convertToDecimal(args[2], 2);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 16));
+                        return;
+                    }
+                    case "--dec": {
+                        String dec = convertToDecimal(args[2], 2);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 10));
+                        return;
+                    }
+                    case "--oct": {
+                        String dec = convertToDecimal(args[2], 2);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 8));
+                        return;
+                    }
+                    case "--bin": {
+                        String dec = convertToDecimal(args[2], 2);
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 2));
+                        return;
+                    }
+                    case "--base": {
+                        if(!args[4].isBlank()){
+                            String dec = convertToDecimal(args[2], 2);
+                            System.out.println(convertDecimal(Integer.valueOf(dec), Integer.valueOf(args[4])));
+                            return;
+                        }else{
+                            error = "Sintaxe invalida --help para mais informações";
+                            break;
+                        }
+                    }
+                
+                    default:
+                        error = "Sintaxe invalida --help para mais informações";
+                    break;
+                }
+            break;
+
+            case "--base":
+
+                if (!args[3].isBlank() || !args[4].isBlank() || !args[5].isBlank()) {
+                    error = "Sintaxe invalida --help para mais informações";
+                }
+
+                
+                switch (args[4]) {
+                    case "--hex": {
+
+                        String dec = convertToDecimal(args[3], Integer.valueOf(args[2]));
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 16));
+                        return;
+                    }
+                    case "--dec": {
+                        String dec = convertToDecimal(args[3], Integer.valueOf(args[2]));
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 10));
+                        return;
+                    }
+                    case "--oct": {
+                        String dec = convertToDecimal(args[3], Integer.valueOf(args[2]));
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 8));
+                        return;
+                    }
+                    case "--bin": {
+                        String dec = convertToDecimal(args[3], Integer.valueOf(args[2]));
+                        System.out.println(convertDecimal(Integer.valueOf(dec), 2));
+                        return;
+                    }
+                    case "--base": {
+                        if(!args[5].isBlank()){
+                            String dec = convertToDecimal(args[3], Integer.valueOf(args[2]));
+                            System.out.println(convertDecimal(Integer.valueOf(dec), Integer.valueOf(args[5])));
+                            return;
+                        }else{
+                            error = "Sintaxe invalida --help para mais informações";
+                            break;
+                        }
+                    }
+                
+                    default:
+                        error = "Sintaxe invalida --help para mais informações";
+                    break;
+                }
+            break;
+
+            case "--values":
+
+                if (!args[2].isBlank()) {
+                    error = "Sintaxe invalida --help para mais informações";
+                }else{
+                    System.out.println(values(Integer.valueOf(args[2])));
+                    return;
+                }
+
+            break;
+        
+            default:
+
+                if (!args[2].isBlank()) {
+                    error = "Sintaxe invalida --help para mais informações";
+                }
+
+                
+                switch (args[2]) {
+                    case "--hex": {
+
+                        System.out.println(convertDecimal(Integer.valueOf(args[1]), 16));
+                        return;
+                    }
+                    case "--dec": {
+                        System.out.println(convertDecimal(Integer.valueOf(args[1]), 10));
+                        return;
+                    }
+                    case "--oct": {
+                        System.out.println(convertDecimal(Integer.valueOf(args[1]), 8));
+                        return;
+                    }
+                    case "--bin": {
+                        System.out.println(convertDecimal(Integer.valueOf(args[1]), 2));
+                        return;
+                    }
+                    case "--base": {
+                        if(!args[3].isBlank()){
+                            System.out.println(convertDecimal(Integer.valueOf(args[1]), Integer.valueOf(args[3])));
+                        return;
+                        }else{
+                            error = "Sintaxe invalida --help para mais informações";
+                            break;
+                        }
+                    }
+                
+                    default:
+                        error = "Sintaxe invalida --help para mais informações";
+                    break;
+                }
+            break;
         }
 
         if (!error.isBlank()){
@@ -52,6 +310,38 @@ public class ConvertComand implements Comando {
         }
 
         return String.valueOf((char) ('A' + x - 10));
+    }
+
+    private static Integer convertCharToNumber(char x){
+        if (x >= '0' && x <= '9') {
+            return x - '0';
+        }else{
+            return (x - 'A') + 10;
+        }
+    }
+
+    private static String convertToDecimal(String in, Integer base){
+        List<Integer> numeros = new ArrayList<>();
+
+        in = in.trim();
+        for (int i = 0; i < in.length() ; i++) {
+            if(convertCharToNumber(in.charAt(i)) < base){
+                numeros.add(convertCharToNumber(in.charAt(i)));
+            }else{
+                throw new IllegalArgumentException("Entrada invalida para a base especificada: " + base);
+            }
+        }
+
+        Integer resultado = 0;
+
+        int e = 0;
+
+        for (int i = numeros.size() - 1; i >= 0; i --) {
+            resultado += numeros.get(i) * (int) Math.pow(base, e);
+            e++;
+        }
+
+        return resultado.toString();
     }
 
     private static String convertDecimal(Integer value, Integer base){
